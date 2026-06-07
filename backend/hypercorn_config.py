ccaf0c728cd7df5b62d416d28ea880afb441919c
WDaily <daily21w@gmail.com>
2026-06-08 02:33:11 +0300

    Update hypercorn_config.py

--- backend/hypercorn_config.py
+++ backend/hypercorn_config.py
@@ -1,15 +1,8 @@
 import os
 import multiprocessing
-import sys
 
 port = os.environ.get("PORT", "8080")
-bind = [f"localhost:{port}"]
-
+bind = [f"0.0.0.0:{port}"]
 workers = multiprocessing.cpu_count()
-
-if sys.platform == "win32":
-	worker_class = "asyncio"
-else:
-	worker_class = "uvloop"
-	
+worker_class = "uvloop"	
 keep_alive_timeout = 5.0
